e84616e5647251f50e900b82a1320dec4bcf4b8f
MatiasGLR <matiasgleivas@gmail.com>
2024-03-13 20:00:06 -0300

    aaa

--- files/js/jpop.js
+++ files/js/jpop.js
@@ -1,29 +1,4 @@
 
-
-
-  function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
-        setCookie("username", user, 365);
-      }
-    }
-  }
-
-  function hash(string) {
-    const utf8 = new TextEncoder().encode(string);
-    return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
-      const hashArray = Array.from(new Uint8Array(hashBuffer));
-      const hashHex = hashArray
-        .map((bytes) => bytes.toString(16).padStart(2, '0'))
-        .join('');
-      return hashHex;
-    });
-  }
-
   function login() {
     var un = $('#username').val();
     var pw = $('#password').val();
@@ -33,25 +8,20 @@
         $('#exampleModal').modal('hide');
         alert ("Te has conectado como administrador.");
         Cookies.set('isadmin','12345',{expires: 5});
-        alert ("Cookie: " + Cookies.get('isadmin') + "");
         return false;
     }
     else {
-        alert ("Login was unsuccessful, please check your username and password");
+        alert ("Usuario y contraseña incorrectos");
     }
   }
 
-  window = function() {isAdmin()};
-  window = function() {isAdmin()};
-
   document.addEventListener("DOMContentLoaded", (event) => {
-    isAdmin();
+    if(isAdmin()){
+
+    }
   });
 
   function isAdmin(){
-    if(Cookies.get('isadmin') == "12345") {
-        document.getElementById("ingresar").textContent = "Administrador";
-    } else {
-        alert('No eres administrador (Cookie: ' + Cookies.get("isadmin") + ')');
-    }
+    if(Cookies.get('isadmin') == "12345") return true;
+    else return false;
   }
