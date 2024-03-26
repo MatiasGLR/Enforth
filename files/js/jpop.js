02c7d648b008a91e324af1c628f5cb78342d4fe6
MatiasGLR <matiasgleivas@gmail.com>
2024-03-26 12:08:30 -0300

    a

--- files/js/jpop.js
+++ files/js/jpop.js
@@ -43,3 +43,7 @@
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+  function bugcoll(id) {
+    $(id).children().collapse('hide');
+  }
